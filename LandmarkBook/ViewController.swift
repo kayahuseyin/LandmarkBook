99b50c7827b1bd4c1e7351b59430e91fdf1e8084
Hüseyin Kaya <kkayahuseyin@hotmail.com>
2022-04-22 00:20:27 +0300

    Initial Commit

---
+++ LandmarkBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LandmarkBook
+//
+//  Created by Hüseyin Kaya on 22.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
